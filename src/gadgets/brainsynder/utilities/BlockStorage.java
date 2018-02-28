8026e6252a0482da9738b689790b57a1bfad1a70
brainsynder <briansnyder498@gmail.com>
2018-02-28 12:48:40 -0500

    Small changes to the Block Utilities

--- src/gadgets/brainsynder/utilities/BlockStorage.java
+++ src/gadgets/brainsynder/utilities/BlockStorage.java
@@ -2,19 +2,39 @@ package gadgets.brainsynder.utilities;
 
 import org.bukkit.block.Block;
 
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
 
 public class BlockStorage {
-    private LinkedList<BlockSave> compoundList = new LinkedList<>();
+    private LinkedHashMap<String, BlockInfo> compoundList = new LinkedHashMap<>();
 
     public void addBlock(Block block) {
-        compoundList.addLast(new BlockSave(block));
+        compoundList.put(new BlockLocation(block.getLocation()).toDataString(), new BlockInfo(block));
+    }
+
+    public BlockInfo getBlockInfo(Block block) {
+        return compoundList.getOrDefault(new BlockLocation(block.getLocation()).toDataString(), new BlockInfo(block));
+    }
+
+    public boolean isEmpty () {
+        return compoundList.isEmpty();
+    }
+
+    public boolean contains (Block block) {
+        return compoundList.containsKey(new BlockLocation(block.getLocation()).toDataString());
     }
 
     public void reset() {
         if (compoundList.isEmpty()) return;
-        for (BlockSave save : compoundList) {
-            save.placeOriginal();
-        }
+        compoundList.values().forEach(BlockInfo::placeOriginal);
+        compoundList.clear();
+    }
+
+    public void reset(Block block) {
+        if (compoundList.isEmpty()) return;
+        if (!contains(block)) return;
+        BlockInfo save = getBlockInfo(block);
+        save.placeOriginal();
+        compoundList.remove(save.getLocation().toDataString());
     }
 }
+
