9ed76ffaab28566c9c8f2e53152eee676696e7fa
brainsynder <briansnyder498@gmail.com>
2018-02-27 23:53:21 -0500

    Added 'ConfettiCannon' and 'Explosive Snowball'

---
+++ src/gadgets/brainsynder/api/gadget/list/ConfettiCannon.java
@@ -0,0 +1,55 @@
+package gadgets.brainsynder.api.gadget.list;
+
+import gadgets.brainsynder.api.GadgetPlugin;
+import gadgets.brainsynder.api.gadget.Gadget;
+import gadgets.brainsynder.api.user.User;
+import gadgets.brainsynder.utilities.ItemBuilder;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import simple.brainsynder.api.ParticleMaker;
+import simple.brainsynder.sound.SoundMaker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConfettiCannon extends Gadget {
+    private List<ParticleMaker> particles = new ArrayList<>();
+
+    public ConfettiCannon(GadgetPlugin plugin) {
+        super(plugin, "confetti");
+        ParticleMaker maker = new ParticleMaker(ParticleMaker.Particle.ITEM_CRACK, 10, 0.3F, 0.3F, 0.3F);
+        for (int data : Arrays.asList(1,15,10,4,11,14,12)) {
+            maker.setData(Material.INK_SACK, (short) data);
+            particles.add(maker);
+        }
+    }
+
+    @Override
+    public void run(User user) {
+        Player player = user.getPlayer();
+
+        Location localLocation1 = player.getEyeLocation();
+        double x = localLocation1.getX();
+        double y = localLocation1.getY();
+        double z = localLocation1.getZ();
+        double yaw = Math.toRadians(localLocation1.getYaw() + 90.0F);
+        double pitch = Math.toRadians(localLocation1.getPitch() + 90.0F);
+        double cos = Math.sin(pitch) * Math.cos(yaw);
+        double sin = Math.sin(pitch) * Math.sin(yaw);
+        double cosP = Math.cos(pitch);
+
+        for (double i = 0.9; i < 2.5; i += 0.1) {
+            Location localLocation2 = new Location(player.getWorld(), x + i * cos, (y + i * cosP), z + i * sin);
+            if (localLocation2.getBlock().getType().isSolid()) break;
+            particles.forEach(maker -> maker.sendToLocation(localLocation2));
+        }
+        SoundMaker.ENTITY_EXPERIENCE_ORB_PICKUP.playSound(player.getLocation());
+    }
+
+    @Override
+    public ItemBuilder getDefaultItem() {
+        return new ItemBuilder(Material.DIAMOND_BARDING, 1).withName("&eConfetti Cannon");
+    }
+}
---
+++ src/gadgets/brainsynder/api/gadget/list/ExplosiveSnowball.java
@@ -0,0 +1,36 @@
+package gadgets.brainsynder.api.gadget.list;
+
+import gadgets.brainsynder.api.GadgetPlugin;
+import gadgets.brainsynder.api.gadget.Gadget;
+import gadgets.brainsynder.api.user.User;
+import gadgets.brainsynder.utilities.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import simple.brainsynder.api.ParticleMaker;
+import simple.brainsynder.sound.SoundMaker;
+
+public class ExplosiveSnowball extends Gadget {
+    public ExplosiveSnowball(GadgetPlugin plugin) {
+        super(plugin, "explosive_snowball");
+    }
+
+    @Override
+    public void run(User user) {
+        getPlugin().getEntityUtils().launchProjectile(user.getPlayer(), Snowball.class, 2.5);
+    }
+
+    @Override
+    public void onProjectileHit(User user, Projectile projectile) {
+        SoundMaker.ENTITY_GENERIC_EXPLODE.playSound(projectile.getLocation());
+        new ParticleMaker(ParticleMaker.Particle.EXPLOSION_HUGE, 40,
+                2.0F, 2.0F, 2.0F).sendToLocation(projectile.getLocation());
+        new ParticleMaker(ParticleMaker.Particle.FLAME, 10,
+                2.0F, 2.0F, 2.0F).sendToLocation(projectile.getLocation());
+    }
+
+    @Override
+    public ItemBuilder getDefaultItem() {
+        return new ItemBuilder(Material.SNOW_BALL, 1).withName("&eExplosive Snowball");
+    }
+}
